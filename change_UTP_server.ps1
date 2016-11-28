f4f8fca392dcc53cb871098bf99136f9d4d59d73
Oleh Makiienko <masterjinn@volia.ua>
2016-11-28 11:42:56 +0200

    Added new script
    Added new script fro SCCM which changes string in ibases.v8i for 1C

---
+++ change_UTP_server.ps1
@@ -0,0 +1,20 @@
+﻿cd $env:userprofile
+$tset = test-path .\Appdata\Roaming\1C\1CEStart\ibases.v8i
+if ($tset -eq $true)
+{
+    $a = Get-Content .\Appdata\Roaming\1C\1CEStart\ibases.v8i
+    $b = $a -contains ‘Connect=Srvr="1c02";Ref="UTP";’
+    if ($tset -eq $true -and $b -eq $true)
+    {
+        Write-Host “Installed”
+        exit 0
+    }
+    else
+    {
+        (Get-Content .\Appdata\Roaming\1C\1CEStart\ibases.v8i) -replace 'Connect=Srvr="1c01";Ref="UTP";', 'Connect=Srvr="1c02";Ref="UTP";' | Set-Content .\Appdata\Roaming\1C\1CEStart\ibases.v8i -Encoding UTF8
+    }
+}
+else
+{
+    exit 0
+}
